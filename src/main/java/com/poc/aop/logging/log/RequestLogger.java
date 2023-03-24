d109adf0ff413fc681dbb8b009f19036d259e62f
mozes.hajdu <mozes.hajdu@areus.hu>
2023-03-24 15:10:51 +0100

    log controllers

---
+++ src/main/java/com/poc/aop/logging/log/RequestLogger.java
@@ -0,0 +1,34 @@
+package com.poc.aop.logging.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class RequestLogger {
+    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @Before("springBeanPointcut()")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(pointcut = "springBeanPointcut()", returning = "response")
+    public void logAfter(JoinPoint joinPoint, ResponseEntity<?> response) {
+        log.info("Exit: {}.{}() with status code = {} and body = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), response.getStatusCode(), response.getBody());
+    }
+}
